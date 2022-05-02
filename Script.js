bc8ba4ef98321381b2450798f691875cff1064ab
foforane <thakgalang.cyber@gmail.com>
2022-05-03 00:12:42 +0200

    size of picture change

--- Script.js
+++ Script.js
@@ -1,3 +1,10 @@
+//AIzaSyCnoSUwanzO4LkvKJpRh-rszFK0GywIOX4
+$("#unjalo").click(function() {
+  window.location.href = 'https://prosiradj.bandcamp.com/track/umuhle-unjalo-reloaded';
+});
+$("#bully").click(function() {
+  window.location.href = 'https://ampl.ink/4oj8V';
+});
 window.onscroll = function() {myFunction()};
 
 var header = document.getElementById("myHeader");
